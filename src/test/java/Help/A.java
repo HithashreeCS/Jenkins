9e9c69f8c707f3f0ef23ae9dc11d31602aa9c62a
HithashreeCS <hithashreecs06@gmail.com>
2024-08-26 15:28:09 +0530

    Master first

--- src/test/java/Help/A.java
+++ src/test/java/Help/A.java
@@ -4,5 +4,7 @@ public class A {
    
 	public static void main(String[] args) {
 		System.out.println("Hitha");
+		System.out.println("Hitha from hassan");
+		System.out.println("Hitha from master fist");
 	}
 }
